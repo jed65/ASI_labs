85df2d78e906168bf2d4379a64f7e4e3387f5a61
jed65 <114737859+jed65@users.noreply.github.com>
2022-11-14 17:13:20 +0000

    Completed Q2

--- Week 5/Lab5Script_Jake.R
+++ Week 5/Lab5Script_Jake.R
@@ -1,50 +1,94 @@
 #Read in the data, 20 obs. of one variable
 dat_gamma<-read.table(url("https://people.bath.ac.uk/kai21/ASI/data/gamma_sample.txt"),header = T)
 
-#Explicit functions for likelihood and gradient with theta=(alpha,beta) (shape and rate parameters)
-nll_fn<-function(theta=c(0,0),data=1){
+
+#### Reparametrisation and optimisation ####
+#Consider parametrisation given by lambda=(log(alpha),log(beta))
+#This takes onto the unconstrained real domain, where we can use optim()
+
+nll_lambda<-function(lambda,data){ #Compute negative log-likelihood
   logdata<-log(data)
-  nll<- -20*theta[1]*log(theta[2])+20*lgamma(theta[1])-(theta[1]-1)*sum(logdata)+theta[2]*sum(data)
+  n<-max(dim(data))
+  nll<- -n*lambda[2]*exp(lambda[1])+n*lgamma(exp(lambda[1]))-(exp(lambda[1])-1)*sum(logdata)+exp(lambda[2])*sum(data)
   return(nll)
 }
 
-nll_grad<-function(theta=c(0,0),data=1){
+nll_lambda_grad<-function(lambda,data){ #Compute gradient of neg. log-likelihood
   logdata<-log(data)
-  nll_grad_theta1<- -20*log(theta[2])+20*digamma(theta[1])-sum(logdata)
-  nll_grad_theta2<- -20*(theta[1]/theta[2])+sum(data)
-  grad_vector<-matrix(0,nrow=2,ncol=1)
-  grad_vector[1]=nll_grad_theta1
-  grad_vector[2]=nll_grad_theta2
-  return(grad_vector)
+  n<-max(dim(data))
+  grad<-matrix(0,nrow=2,ncol=1)
+  grad[1]<- -n*lambda[2]*exp(lambda[1])+n*exp(lambda[1])*digamma(exp(lambda[1]))-exp(lambda[1])*sum(logdata)
+  grad[2]<- -n*exp(lambda[1])+exp(lambda[2])*sum(data)
+  return(grad)
 }
 
-#First we calculate an asymptotic confidence interval
-#Now use optim to fit to the data
-fit_alpha_beta<-optim(par=c(0.5,0.5),fn=nll_fn,gr=nll_grad,
-                      data=dat_gamma,method="BFGS",hessian=TRUE)
+#Now use optim to minimise function with respect to unconstrained parameters
+fit_lambda<-optim(par     = c(0,0),
+                  fn      = nll_lambda,
+                  gr      = nll_lambda_grad,
+                  data    = dat_gamma,
+                  method = "BFGS",
+                  hessian = TRUE)
+#Gives us the MLE for lambda, which can be transformed back into (alpha,beta)
+lambda_star<-fit_lambda$par
+shape_rate_star<-exp(lambda_star)
 
-fisher_shape_rate_observed<-fit_alpha_beta$hessian #use observed information
-inverse_fisher_shape_rate_observed<-solve(fisher_shape_rate_observed) #find inverse
+#Can also find Hessian of lambda and using Jacobian, that of (alpha,beta)
+#Note that in gamma distribution example, Hessian and fisher information are equal
+hess_lambda_observed<-fit_lambda$hessian
+J<-diag(exp(-c(fit_lambda$par))) #This is the Jacobian, note the symmetry and remember we are going back from lambda to theta!
+hess_theta_observed<-t(J)%*%hess_lambda_observed%*%J 
 
-#Now consider reparametrisation given by g_(1)^(-1)(alpha,beta)=alpha-beta^2 and g_(1)^(-1)(alpha,beta)=beta
-#MLE for alpha-beta^2
-lambda_1_star<-fit_alpha_beta$par[1]-fit_alpha_beta$par[2]^2
+#Can get inverse of this observed fisher info matrix (in terms of (alpha,beta))
+inverse_fisher_theta<-solve(hess_theta_observed) #Has reasonable values
 
-#Inverse fisher info matrix
-J_g_inverse=matrix(0,nrow=2,ncol=2)
-J_g_inverse[1,1]=1; J_g_inverse[2,2]=1; J_g_inverse[1,2]=-2*fit_alpha_beta$par[2]
-#Find reparametrised inverse fisher matrix
-inverse_fisher_reparametrised<-J_g_inverse%*%inverse_fisher_shape_rate_observed%*%t(J_g_inverse)
+#Summarising, we have inverse observed fisher and MLE for theta=(alpha,beta)
 
-#Finally, find the confidence interval
-lower_bound<-lambda_1_star-qnorm(0.025,lower.tail = FALSE)*sqrt(inverse_fisher_reparametrised[1,1])
-upper_bound<-lambda_1_star+qnorm(0.025,lower.tail = FALSE)*sqrt(inverse_fisher_reparametrised[1,1])
-
-
-#Print to screen
-lower_bound
-upper_bound
-#Gives the interval (-14.60623,8.120496), which contains 0 so evidence supporting null hypothesis
+#### Hypothesis test - Confidence intervals ####
+#For hypothesis test, use reparametrisation gamma=(alpha-beta^2,beta)
+MLE<-c(shape_rate_star[1]-shape_rate_star[2]^2,shape_rate_star[2]) #find reparametrised MLE
+J_2<-matrix(0,nrow=2,ncol=2) #Form Jacobian in this case
+J_2<-J_2+diag(c(1,1))
+J_2[1,2]=2*shape_rate_star[2]
+hess_gamma_observed<-t(J_2)%*%hess_theta_observed%*%J_2 #Find reparametrized Fisher info
+inverse_fisher_gamma<-solve(hess_gamma_observed)
 
 
-#Now we need to consider the generalised likelihood ratio test
+#We now have all the ingredients required for hypothesis test
+lower_bound=MLE[1]-qnorm(0.025,lower.tail = FALSE)*sqrt(inverse_fisher_gamma[1,1])
+upper_bound=MLE[1]+qnorm(0.025,lower.tail = FALSE)*sqrt(inverse_fisher_gamma[1,1])
+confidence_interval<-c(lower_bound,upper_bound)
+confidence_interval
+#This contains zero, insufficient evidence to reject null hypothesis
+
+#### Hypothesis test - GLRT ####
+#Use invariance property of MLE for unrestrained problem, remember GLRT uses log-likelihood (x-1)
+MLE_likelihood_unrestrained<- -nll_lambda(fit_lambda$par,dat_gamma)
+
+#Need to define negative log-likelihood and gradient in restrained case
+#Note that we still have to reparametrise to use optim()
+#First function is for negative log-likelihood, second is for gradient
+nll_restrained<-function(lambda,data){
+  logdata<-log(data)
+  n<-max(dim(data))
+  nll<- -(n/2)*lambda*exp(lambda)+n*lgamma(exp(lambda))-(exp(lambda)-1)*sum(logdata)+exp(lambda/2)*sum(data)
+}
+
+nll_restrained_gradient<-function(lambda,data){
+  logdata<-log(data)
+  n<-max(dim(data))
+  grad<- -(n/2)*lambda*exp(lambda)-(n/2)*exp(lambda)+n*exp(lambda)*digamma(exp(lambda))-exp(lambda)*sum(logdata)+0.5*exp(lambda/2)*sum(data)
+  return(grad)
+}
+
+#Now optimise for restrained MLE and get value of negative log-likelihood at this point
+fit_lambda_1<-optim(par   = 0,
+                  fn      = nll_restrained,
+                  gr      = nll_restrained_gradient,
+                  data    = dat_gamma,
+                  method = "BFGS")
+MLE_likelihood_restrained<- -nll_restrained(fit_lambda_1$par,dat_gamma)
+
+test_statistic<-2*(MLE_likelihood_unrestrained-MLE_likelihood_restrained) #small value of t
+critical_value<-qchisq(p=.95,df=1)
+test_statistic>critical_value #this is false, we have insufficient evidence to reject null as before
